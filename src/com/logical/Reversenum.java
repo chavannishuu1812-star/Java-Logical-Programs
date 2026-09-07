6da8bf9268b783177a1fbdef67b307e29fbb9e08
Nishu <chavannishuu1812@gmail.com>
2026-09-07 12:35:05 +0530

    Day 1 - Reverse a Number

---
+++ src/com/logical/Reversenum.java
@@ -0,0 +1,19 @@
+package com.logical;
+
+public class Reversenum {
+
+	    public static void main(String[] args) {
+
+	        int num = 1234;
+	        int reverse = 0;
+
+	        while (num > 0) {
+	            int digit = num % 10;
+	            reverse = reverse * 10 + digit;
+	            num = num / 10;
+	        }
+
+	        System.out.println(reverse);
+	    }
+	}
+
