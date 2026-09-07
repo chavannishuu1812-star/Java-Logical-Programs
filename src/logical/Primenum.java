c1d01e692cc2fdf6b0fc692aa6e894e7331c3df9
Nishu <chavannishuu1812@gmail.com>
2026-09-07 13:55:52 +0530

    added Primenum

---
+++ src/logical/Primenum.java
@@ -0,0 +1,29 @@
+package logical;
+
+public class Primenum {
+
+
+	    public static void main(String[] args) {
+
+	        int num = 17;
+	        boolean isPrime = true;
+
+	        if (num <= 1) {
+	            isPrime = false;
+	        }
+
+	        for (int i = 2; i < num; i++) {
+	            if (num % i == 0) {
+	                isPrime = false;
+	                break;
+	            }
+	        }
+
+	        if (isPrime) {
+	            System.out.println("Prime Number");
+	        } else {
+	            System.out.println("Not a Prime Number");
+	        }
+	    }
+	}
+
