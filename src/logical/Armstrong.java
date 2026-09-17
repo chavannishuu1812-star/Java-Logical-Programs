519f6fe3286a50e928c2ed598b8221f03dc6b6d7
Nishu <chavannishuu1812@gmail.com>
2026-09-17 15:05:29 +0530

    added armstrong program

---
+++ src/logical/Armstrong.java
@@ -0,0 +1,26 @@
+package logical;
+
+public class Armstrong {
+	
+
+	    public static void main(String[] args) {
+
+	        int num = 153;
+	        int original = num;
+	        int sum = 0;
+
+	        while (num > 0) {
+	            int digit = num % 10;
+	            sum = sum + (digit * digit * digit);
+	            num = num / 10;
+	        }
+
+	        if (original == sum) {
+	            System.out.println("Armstrong Number");
+	        } else {
+	            System.out.println("Not an Armstrong Number");
+	        }
+	    }
+	}
+
+
